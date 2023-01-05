8eb3b9f81ca53494286945e5dfaea789da72b292
James Olsen <olsenjames1116@gmail.com>
2023-01-05 14:59:09 -0500

    Assign variables to user input and display values
    The variables were assigned to the user input to run through the
    constructor and create a book object. The values were displayed to the
    console to ensure the variables were assigned correctly.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
+const title = document.querySelector('input#title');
+const author = document.querySelector('input#author');
+const pages = document.querySelector('input#pages');
+const read = document.querySelector('input#read');
+const submitButton = document.querySelector('form>button[type=button]');
+
+submitButton.addEventListener('click', (event) => {
+    console.log(`${title.value} ${author.value} ${pages.value} ${read.value}`);
+});
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
