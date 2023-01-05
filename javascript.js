d6e4d441babf42723410d12dd6c001ccf09513bc
James Olsen <olsenjames1116@gmail.com>
2023-01-05 13:46:53 -0500

    Install Prettier and ESLint

--- javascript.js
+++ javascript.js
@@ -1,10 +1,15 @@
 function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.info = function() {
-    return `${title} by ${author}, ${pages} pages, ${read} yet`;
-  }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.info = function () {
+        return `${title} by ${author}, ${pages} pages, ${read} yet`;
+    };
 }
 
-const theHobbit = new Book('The Hobbit', 'J R R Tolkien', 290, 'not read');
+const leviathanWakes = new Book(
+    'Leviathan Wakes',
+    'James S. A. Corey',
+    577,
+    true
+);
